a3c9759d329965d83d02b05b3b1ba0b8160abf96
kristian0808 <kristianlleshaj@hotmail.com>
2024-04-17 12:34:05 +0200

    updated db tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -66,6 +66,24 @@ test_db_storage.py'])
                              "{:s} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
+    def test_get(self):
+        """Test that get method retrieves object by id"""
+        state = State(name="California")
+        state.save()
+        retrieved_state = storage.get(State, state.id)
+        self.assertEqual(state.id, retrieved_state.id)
+        self.assertIs(state, retrieved_state)
+
+    def test_count(self):
+        """Test that count method returns correct number of objects"""
+        initial_count = storage.count()
+        state1 = State(name="California")
+        state1.save()
+        state2 = State(name="New York")
+        state2.save()
+
+        self.assertEqual(storage.count(), initial_count + 2)
+        self.assertEqual(storage.count(State), initial_count + 2)
 
 
 class TestFileStorage(unittest.TestCase):
